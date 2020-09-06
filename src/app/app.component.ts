81abd0c63ef319e90abf59bbddbab5ec73f95839
Eldor901 <eldor9012545@gmail.com>
2020-09-06 21:27:45 +0300

    basic tutorial angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export interface Card {
+  title: string
+  text: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learn-Angular';
+  togle = true;
+
+  cards: Card[] = [
+    {title: 'Card ', text: 'This is card number 1'},
+    {title: 'Card 2', text: 'This is card number 2'},
+    {title: 'Card 3', text: 'This is card number 3'},
+  ];
+
+  togleCards() {
+    this.togle = !this.togle;
+  }
+
 }

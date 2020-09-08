8bdefd69861d57c654c5179a02d741fc9b6f3e9e
Eldor901 <eldor9012545@gmail.com>
2020-09-08 03:09:21 +0300

    Pipes and post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pipes';
+  search = '';
+  searchField = 'title';
+
+
+  posts: Post[] = [
+    {title: "Somsa", text: "Ya lubli somasu"},
+    {title: "Nyam Nyam", text: "woooooooooow"},
+    {title: "Nyam Nyam", text: "woooooooooow"},
+  ];
+
+  addPost() {
+    this.posts.push({
+      title: 'Angular 9',
+      text: 'Course wooooow'
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {MultByPipe} from './Pipes/mult-by.pipe';
+import { ExMarksPipe } from './pipes/ex-marks.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, MultByPipe, ExMarksPipe, FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
